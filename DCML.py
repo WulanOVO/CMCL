04795ba7dd959b85519423d43fbf63130245efa7
WulanOVO <133559595+WulanOVO@users.noreply.github.com>
2023-10-09 18:41:11 +0800

    v3.1

--- DCML.py
+++ DCML.py
@@ -1,18 +1,18 @@
-from os import path,remove,mkdir,listdir
+from os import path,remove,mkdir,listdir,system
 from shutil import rmtree,unpack_archive
 from locale import getdefaultlocale
 from psutil import virtual_memory
-from subprocess import run
 from random import randint
 from wget import download
 from requests import get
-from time import sleep
 from json import loads
 
 #获取系统语言#
 lang,_ = getdefaultlocale()
 lang = lang.lower()
+system('cls')
 
+#初始化#
 if not path.exists('cmcl.json'): #判断第一次启动#
     #生成玩家名#
     name = 'Player'+str(randint(1000,9999))
@@ -20,29 +20,28 @@ if not path.exists('cmcl.json'): #判断第一次启动#
         print('欢迎使用DCML-大聪明启动器！\n你的游戏名是'+name)
     else:
         print('Welcome to use DCML!\nYour player name is '+name)
-    sleep(3)
-    run('cls',shell=True)
+
+    #下载cmcl#
+    download('https://gitee.com/MrShiehX/console-minecraft-launcher/releases/download/2.2.1/cmcl.jar',bar=None)
+    system('cls')
 
     try:
         #下载Java并解压#
-        download('https://d6.injdk.cn/openjdk/openjdk/20/openjdk-20.0.2_windows-x64_bin.zip')
-        unpack_archive('openjdk-20.0.2_windows-x64_bin.zip')
-        remove('openjdk-20.0.2_windows-x64_bin.zip')
-        #下载cmcl#
-        download('https://gitee.com/MrShiehX/console-minecraft-launcher/releases/download/2.2/cmcl.jar')
+        download('https://d6.injdk.cn/openjdk/openjdk/21/openjdk-21_windows-x64_bin.zip')
+        unpack_archive('openjdk-21_windows-x64_bin.zip')
+        remove('openjdk-21_windows-x64_bin.zip')
     except:
         #无网络时报错并退出#
         if lang == 'zh_cn':
             print('下载出错，请检测网络或稍后重试')
         else:
             print('Download error, please check the network or try again later')
-        sleep(3)
-        exit(0)
+        while True:pass
 
     #手动创建cmcl.json#
     open('cmcl.json', 'w').write('''{
 "exitWithMinecraft": false,
-"javaPath": "jdk-20.0.2//bin//java.exe",
+"javaPath": "jdk-21//bin//java.exe",
 "downloadSource": 2,
 "checkAccountBeforeStart": false,
 "accounts": [{
@@ -53,12 +52,13 @@ if not path.exists('cmcl.json'): #判断第一次启动#
 "printStartupInfo": false,
 "maxMemory": 8192
 }''' %name)
-    run('cls',shell=True)
+    system('cls')
 
-def check(): #检测是否需要更新#
+#检测是否需要更新#
+def check():
     global nV
     global nOF
-    h = {'User-Agent':'Chrome/51.0.2704.63'}
+    h = {'User-Agent':'Mozilla/5.0'}
     first = False
     noOF = False
     vL = []
@@ -84,34 +84,48 @@ def check(): #检测是否需要更新#
         oV = old[0]
         oOF = old[1]
     except:
-        first = True #标记第一次启动（没有以安装版本）#
+        first = True #标记第一次启动（没有已安装版本）#
 
-    if first and noOF: #第一次启动&OF没更新#
+    #第一次启动,OF没更新#
+    if first and noOF:
         nV = vL[1] #下载版本改为上一个版本#
-        nOF = nOF[0]['type']+'_'+nOF[0]['patch'] #重新获取OF版本#
+        nOF = get('https://download.mcbbs.net/optifine/'+nV,headers=h).json() #重新获取OF版本#
+        nOF = nOF[0]['type']+'_'+nOF[0]['patch']
         return True
-    elif first and not noOF: #第一次启动&有OF#
+
+    #第一次启动,有OF#
+    elif first and not noOF:
         return True
-    elif not first and noOF: #不是第一次启动&OF没更新#
+
+    #不是第一次启动,OF没更新#
+    elif not first and noOF:
+        nV = oV #指定已有版本#
+        nOF = oOF
         return False #不更新，等待OF更新新版本#
 
-    if oV != nV or oOF != nOF: #常规判断#
+    #常规判断#
+    if oV != nV or oOF != nOF:
         return True
 
-
+#下载新版本#
 if check():
     try:
         #删除旧版本（不影响存档）#
         rmtree('.minecraft/versions')
         mkdir('.minecraft/versions')
     except:pass
-    #下载新版本（命名格式：游戏版本-OF版本）#
-    run('"jdk-20.0.2/bin/java.exe" -jar cmcl.jar install %s --optifine %s -n %s-%s' %(nV,nOF,nV,nOF),shell=True)
+    #下载#
+    system('jdk-21\\bin\\java.exe -jar cmcl.jar install %s --optifine %s -n %s-%s' %(nV,nOF,nV,nOF))
 
-    if not path.exists('.minecraft\saves'): #判断第一次下载#
-        open('.minecraft\options.txt', 'w').write('lang:'+lang) #自动修改游戏语言#
-
-    run('cls',shell=True)
+    if not path.exists('.minecraft/saves'): #判断第一次下载#
+        open('.minecraft/options.txt', 'w').write('lang:'+lang) #自动修改游戏语言#
+        open('.minecraft/optionsof.txt', 'w').write('''ofSmoothFps:true
+ofRenderRegions:true
+ofSmartAnimations:true
+ofFastMath:true
+ofFastRender:true
+ofChunkUpdatesDynamic:true''') #开启优化设置#
+    system('cls')
 
 try:
     #自动分配内存#
@@ -121,4 +135,4 @@ try:
 except:pass
 
 #我的世界，启动！#
-run('"jdk-20.0.2/bin/java.exe" -jar cmcl.jar %s-%s' %(nV,nOF),shell=True)
+system('"jdk-21\\bin\\java.exe" -jar cmcl.jar %s-%s' %(nV,nOF))
